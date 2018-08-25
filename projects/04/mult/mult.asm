d530150be938c5dd1b6c84b6616e2b8ef9cf0204
ctiller15 <christiller2015@gmail.com>
2018-08-25 19:02:15 -0400

    Start project 4 and create multiplication program

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,36 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@i
+M = 1
+@prod
+M = 0
+@R2
+M = 0
+
+(LOOP)
+@i
+D = M
+@R0
+D = D - M // Checking current incrementor.
+
+@SET
+D; JGT // If D is > 0, go to SET value.
+@R1
+D = M // Setting D to the first register.
+@prod
+M = D + M
+@i
+M = M + 1 // Incrementing the index.
+@LOOP
+0; JMP // Restarts the loop.
+
+(SET)
+@prod
+D = M
+@R2
+M = D
+
+(END)
+@END
+0; JMP
