d85bf15e99d84d28f69aec9f509d6d3935e1fbb1
ctiller15 <christiller2015@gmail.com>
2018-09-01 00:49:46 -0400

    Create memory chip in project 5

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,62 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@i
+M = 0
+@current
+M = 0
+
+(IDLE)
+@KBD
+D = M
+@CLEAR
+D; JEQ
+@FILL
+D; JGT
+@IDLE
+0; JMP
+
+(CLEAR)
+@color
+M = 0
+@LOOP
+0; JMP
+
+(FILL)
+@color
+M = -1
+@LOOP
+0; JMP
+
+(LOOP)
+@i
+D = M
+@8192
+D = D - A
+@END
+D; JGE
+
+@SCREEN
+D = A
+@i
+D = D + M
+
+@current // set the current register.
+M = D
+@color
+D = M
+@current
+A = M
+M = D
+
+@i
+M = M + 1
+@LOOP
+0; JMP // restart the loop.
+
+(END)
+@i
+M = 0 // Reset the index, and then start the process all over again.
+@IDLE
+0; JMP
