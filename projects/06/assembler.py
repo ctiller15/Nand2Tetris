133512e8e246ce944a52b375b5ca5dc02f60d047
ctiller15 <christiller2015@gmail.com>
2018-09-09 13:59:25 -0400

    Begin working on assembler

---
+++ projects/06/assembler.py
@@ -0,0 +1,118 @@
+# This is an assembler made for project 6 of the nand to tetris course.
+import re
+
+# Initializing all relevant dictionaries.
+compAzero = {
+    '0': '101010',
+    '1': '111111',
+    '-1': '111010',
+    'D': '001100',
+    'A': '110000',
+    '!D': '001101',
+    '!A': '110001',
+    '-D': '001111',
+    '-A': '110011',
+    'D+1': '011111',
+    'A+1': '110111',
+    'D-1': '001110',
+    'A-1': '110010',
+    'D+A': '000010',
+    'D-A': '010011',
+    'A-D': '000111',
+    'D&A': '000000',
+    'D|A': '010101',
+}
+
+compAone = {
+    'M': '110000',
+    '!M': '110001',
+    '-M': '110011',
+    'M+1': '110111',
+    'M-1': '110010',
+    'D+M': '000010',
+    'D-M': '010011',
+    'M-D': '000111',
+    'D&M': '000000',
+    'D|M': '010101',
+}
+
+destMap = {
+    'null': '000',
+    'M': '001',
+    'D': '010',
+    'MD': '011',
+    'A': '100',
+    'AM': '101',
+    'AD': '110',
+    'AMD': '111',
+}
+
+jumpMap = {
+    'null': '000',
+    'JGT': '001',
+    'JEQ': '010',
+    'JGE': '011',
+    'JLT': '100',
+    'JNE': '101',
+    'JLE': '110',
+    'JMP': '111',
+}
+
+#Opening file.
+f = open('./add/Add.asm', 'r')
+
+instructions = []
+
+#Clearing out line comments and whitespace.
+for line in f:
+    if(line != "\n" and not(line.startswith('//'))):
+        instructions.append(line.strip())
+
+print(instructions)
+
+# Parsing the individual sections for each instruction.
+
+#First, identify if A instruction or C-instruction.
+
+def HandleAInstruction(instruction):
+    print('A instruction')
+    ainst = '0' + bin(int(instruction[1:]))[2:].zfill(15)
+    return ainst
+
+def HandleCInstruction(instruction):
+    cdjinst = []
+    comp = 'null'
+    dest = 'null'
+    jump = 'null'
+    if('=' in instruction and ';' in instruction):
+        cdjinst = re.split('= |; ', instruction)
+        dest, comp, jump = cdjinst
+    elif('=' in instruction):
+        cdjinst = re.split('=', instruction)
+        dest, comp = cdjinst
+    elif(';' in instruction):
+        cdjinst = re.split(';', instruction)
+        comp, jump = cdjinst
+    else:
+        cdjinst = instruction
+    return f'111{HandleCompInstruction(comp)}{destMap[dest]}{jumpMap[jump]}'
+
+def HandleCompInstruction(instruction):
+    if(instruction[0] in compAzero.keys()):
+        comp = '0' + compAzero[instruction[0]]
+    elif(instruction[0] in compAone.keys()):
+        comp = '1' + compAone[instruction[0]]
+    return comp
+
+#Parser.
+def Parser(instructions):
+    parsed = []
+    for instr in instructions:
+        if(instr.startswith('@')):
+            parsed.append(HandleAInstruction(instr))
+        else:
+            parsed.append(HandleCInstruction(instr))
+    return parsed
+
+parsedInstructions = Parser(instructions)
+print(parsedInstructions)
